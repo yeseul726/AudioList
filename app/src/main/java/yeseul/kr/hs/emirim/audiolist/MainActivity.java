e04f143d544a028c6f11436532a99401558e7d6b
leeysi <yeseul726@e-mirim.hs.kr>
2017-06-19 20:57:34 +0900

    #3 ListView ArrayList setting

--- app/src/main/java/yeseul/kr/hs/emirim/audiolist/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/audiolist/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         butPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mediaPlayer.create(MainActivity.this, selectedMusicId);
                 mediaPlayer.start();
             }
         });
